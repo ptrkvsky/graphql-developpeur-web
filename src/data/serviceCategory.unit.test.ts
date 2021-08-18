3f96d5b29313275c1a832fd7cb1228157284040b
ptrkvsky <johan.petrikovsky@gmail.com>
2021-08-18 07:58:51 +0200

    test(category): implement CRUD test for category

--- src/data/serviceCategory.unit.test.ts
+++ src/data/serviceCategory.unit.test.ts
@@ -1,13 +1,30 @@
 import { Category } from '@prisma/client';
 import prismaMock from '@src/__tests__/__mocks__/prismaMock';
-import { createCategory, getCategory } from '@src/data/serviceCategory';
+import {
+  createCategory,
+  getCategory,
+  getAllCategorys,
+  updateCategory,
+  deleteCategory,
+} from '@src/data/serviceCategory';
 
 const mockCategory: Category = {
   id: 1,
   name: 'PHP',
 };
 
+const mockCategory2: Category = {
+  id: 2,
+  name: 'HTML',
+};
+
+const updatedMockCategory: Category = {
+  id: 1,
+  name: 'NodeJS',
+};
+
 describe('Category service tests', () => {
+  // CREATE
   describe('Create category test', () => {
     test('It should create categegory with passed in args', async () => {
       (prismaMock.category.create as jest.Mock).mockResolvedValue(mockCategory);
@@ -22,6 +39,7 @@ describe('Category service tests', () => {
     });
   });
 
+  // GET ONE
   describe('Get category test', () => {
     test('It should get categegory with passed in args', async () => {
       (prismaMock.category.findUnique as jest.Mock).mockResolvedValue(
@@ -29,6 +47,41 @@ describe('Category service tests', () => {
       );
 
       const category = await getCategory(mockCategory.id);
+      expect(category).toBe(mockCategory);
+    });
+  });
+
+  // GET ALL
+  describe('Get all categorys test', () => {
+    test('It should get all categegorys with passed in args', async () => {
+      const mockCategorys = [mockCategory, mockCategory2];
+      (prismaMock.category.findMany as jest.Mock).mockResolvedValue(
+        mockCategorys
+      );
+
+      const categorys = await getAllCategorys();
+      expect(categorys).toBe(mockCategorys);
+    });
+  });
+
+  // UPDATE
+  describe('Update category test', () => {
+    test('It should update the category with passed in args', async () => {
+      (prismaMock.category.update as jest.Mock).mockResolvedValue(
+        updatedMockCategory
+      );
+
+      const category = await updateCategory(1, 'NodeJS');
+      expect(category).toBe(updatedMockCategory);
+    });
+  });
+
+  // DELETE
+  describe('Delete category test', () => {
+    test('It should delete the category with passed in args', async () => {
+      (prismaMock.category.delete as jest.Mock).mockResolvedValue(mockCategory);
+
+      const category = await deleteCategory(1);
       expect(category).toBe(mockCategory);
     });
   });
