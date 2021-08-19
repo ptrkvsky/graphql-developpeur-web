eb18367e9718ebced27b489afbcddd4a4f5d30e6
ptrkvsky <johan.petrikovsky@gmail.com>
2021-08-19 07:12:00 +0200

    refactor: create folders for different modules

--- src/graphql/schema/resolvers/query/post/getPost.ts
+++ src/graphql/schema/resolvers/query/post/getPost.ts
@@ -2,7 +2,7 @@ import { GraphQLFieldConfig } from 'graphql/type/definition';
 import { GraphQLInt } from 'graphql';
 import { Post } from '@prisma/client';
 import { IApolloServerContext } from '@src/lib/interfaces/IApolloServerContext';
-import GqlPost from '@src/graphql/schema/typedefs/GqlPost';
+import GqlPost from '@src/graphql/schema/typedefs/post/GqlPost';
 import { getPost } from '@src/data/postService';
 
 const getPostQuery: GraphQLFieldConfig<unknown, IApolloServerContext> = {
